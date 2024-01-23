16cdea6817456a9aa32cabe1e98cb1da565ac2fb
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-01-23 16:41:32 -0700

    added login screen styling

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -23,16 +23,15 @@ export const LoginForm = () => {
 
     return (
         <Row id='container'>
-            <Col id='login-col'>
-                
-            </Col>
-            <Col className='login-col col-6'>
+            <Col id='left-col'></Col>
+            <Col className='login-col col-8'>
                 <FormContainer>
-                <h1>Welcome Back, Sign In</h1>
+                <h1>Sign In</h1>
                     <Form onSubmit={loginHandler}>
                         <Form.Group controlId='email' className='my-3'>
                             <Form.Label >Email:</Form.Label>
-                                <Form.Control 
+                                <Form.Control
+                                    className='form-inputs'
                                     type='email' 
                                     placeholder='Enter Email' 
                                     value={email} 
@@ -40,19 +39,24 @@ export const LoginForm = () => {
                                 </Form.Control>
                         </Form.Group>
                         <Form.Group controlId='password' className='my-3'>
-                            <Form.Label>Password</Form.Label>
-                                <Form.Control 
+                            <Form.Label className='form-label'>Password</Form.Label>
+                                <Form.Control
+                                    className='form-inputs'
                                     type='password' 
                                     placeholder='Enter Password' 
                                     value={password} 
                                     onChange={(e)=>setPassword(e.target.value)}>
                                 </Form.Control>
                         </Form.Group>
-                        <Button type='submit' variant='primary' className='mt-2' > Sign In </Button>
+                        <Button id='btn' type='submit' variant='primary' className='mt-2' > Sign In </Button>
                     </Form>
                     <Row className='py-3'>
-                        <Col style={{color: '#6987a5'}}>
-                            New Customer? <Link to={'/register'}>Register</Link>
+                        <Col >
+                            <div className='register'>
+                                <p>
+                                    New Customer? <Link to={'/register'} className='link'>Register Here</Link>
+                                </p>
+                            </div>
                         </Col>
                     </Row>
                 </FormContainer>
