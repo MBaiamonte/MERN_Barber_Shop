4e26b9fd56bda627ae64097227520401257b525f
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-01-23 14:04:29 -0700

    delete auth component and remove its warp around react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,16 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { AuthProvider } from './components/AuthContext';
 import HomePage from './views/HomePage';
 import LoginPage from './views/LoginPage'
 
 function App() {
   return (
-    <AuthProvider>
+
       <BrowserRouter>
         <Routes>
           <Route element={<HomePage/>} path="/"/>
           <Route element={<LoginPage/>} path="/login"/>
         </Routes>
       </BrowserRouter>
-    </AuthProvider>
   );
 }
 
