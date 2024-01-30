afa245ea7f9564118b61482bedc08788f2834b1c
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-01-30 16:00:48 -0700

    add global state for logged in user information

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { useState } from 'react';
 import HomePage from './views/HomePage';
 import LoginPage from './views/LoginPage';
 import RegisterForm from './components/RegisterForm';
@@ -6,13 +7,14 @@ import ServicePage from './views/ServicePage';
 import AboutUsPage from './views/AboutUsPage';
 
 function App() {
+  const [loggedUser, setLoggedUser] = useState(null)
   return (
 
       <BrowserRouter>
         <Routes>
-          <Route element={<HomePage/>} path="/"/>
-          <Route element={<LoginPage/>} path="/login"/>
-          <Route element={<RegisterForm/>} path="/register"/>
+          <Route element={<HomePage setLoggedUser={setLoggedUser} loggedUser={loggedUser}/>} path="/"/>
+          <Route element={<LoginPage setLoggedUser={setLoggedUser} />} path="/login"/>
+          <Route element={<RegisterForm setLoggedUser={setLoggedUser}/>} path="/register"/>
           <Route element={<ServicePage/>} path="/services"/>
           <Route element={<AboutUsPage/>} path="/aboutUs"/>
         </Routes>
