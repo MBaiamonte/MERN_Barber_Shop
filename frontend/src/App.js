96520aae88d21a5ac0905458090c5a7772048402
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-02-18 14:13:33 -0700

    add contact us routes and functionality within application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import AboutUsPage from './views/AboutUsPage';
 import DashboardPage from './views/DashboardPage';
 import {UserProvider} from './components/UserContext';
 import AppointmentFormPage from './views/AppointmentFormPage';
+import ContactUsPage from './views/ContactUsPage';
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
             <Route element={<AboutUsPage/>} path="/aboutUs"/>
             <Route element={<DashboardPage />} path='/profile/:userId'/>
             <Route element={<AppointmentFormPage />} path='/appointment/:userId/book'/>
+            <Route element={<ContactUsPage />} path='/contactUs'/>
           </Routes>
           </UserProvider>
       </BrowserRouter>
