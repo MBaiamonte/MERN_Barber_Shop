e0971cf4b2b70296ec0a3cd540f83aa199dc8e38
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-02-22 17:49:33 -0700

    deployment debugging 404 not found

--- server/server.js
+++ server/server.js
@@ -21,15 +21,11 @@ app.use(
     )
 
     //Deployment
-    // app.use(express.static(path.join(__dirname, 'frontend')));
-    // app.get('*', (req,res)=>{
-    //     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    // })
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build'));
-});
 
 //Connect To Database 
 connectDB()
