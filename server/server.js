cb7c6ba49f7631097c7b89349c82c4b668364524
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-02-22 17:26:18 -0700

    deployment debugging

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(
     cookieParser(),
     )
 
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static(path.join(__dirname, 'frontend')));
     app.get('*', (req,res)=>{
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
