1e1d30078199c78b9dabb32d0ed4a0f5af4d7871
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-02-23 15:44:37 -0700

    fixed image routes

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row,Button} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import '../styles/Welcome.css';
 import {useUser} from '../components/UserContext';
@@ -7,18 +7,19 @@ import {useUser} from '../components/UserContext';
 const Welcome = () => {
     //Declarations
     const { loginUserId } = useUser();
+
     return (
         <>
             <Row className='row-1'>
-                    <p className='welcome-p'>
-                        Your only as good as your last Haircut
-                        <p className='welcome-p'>Edward ScissorHands</p>
-                        {loginUserId === null ? (
-                            <Link to='/login'><button className='welcome-btn'>Login</button></Link>
-                        ) : (
-                            <Link to={`/appointment/${loginUserId}/book`}><button className='welcome-btn'>Book Now!</button></Link>
-                        )}
-                    </p>
+                <p className='welcome-p'>
+                    Your only as good as your last Haircut
+                    <p className='welcome-p'>Edward ScissorHands</p>
+                    {loginUserId === null ? (
+                        <Link to='/login'><button className='welcome-btn'>Login</button></Link>
+                    ) : (
+                        <Link to={`/appointment/${loginUserId}/book`}><button className='welcome-btn'>Book Now!</button></Link>
+                    )}
+                </p>
             </Row>
         </>
     )
