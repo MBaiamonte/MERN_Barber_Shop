6fa5b7550ea722256b4f16977659ccad32d0c6f7
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-02-23 16:18:46 -0700

    debugging server when in production vs  development

--- server/server.js
+++ server/server.js
@@ -21,11 +21,16 @@ app.use(
     )
 
     //Deployment
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-    app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    })
-
+    if(process.env.NODE_ENV === 'production'){
+        app.use(express.static(path.join(__dirname, '../frontend/build')));
+        app.get('*', (req,res)=>{
+            res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+        })
+    } else{
+        app.get('/', (req,res)=>{
+            res.send('API is running...')
+        });
+    }
 
 //Connect To Database 
 connectDB()
